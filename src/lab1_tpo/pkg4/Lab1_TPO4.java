2c921cd61e294c4638145545099e4c06903a3ae6
MarBarzola <mmbdcrazyart@gmail.com>
2023-09-10 22:18:13 -0300

    Initial commit

---
+++ src/lab1_tpo/pkg4/Lab1_TPO4.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_tpo.pkg4;
+
+/**
+ *
+ * @author MaR Bd
+ */
+public class Lab1_TPO4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
